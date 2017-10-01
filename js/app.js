31bf8d8ed58ac84a0637f683755c94af3d729412
Katarzyna Galas <katarzyna.galas2@gmail.com>
2017-10-02 00:24:36 +0200

    accordion test

--- js/app.js
+++ js/app.js
@@ -1,4 +1,32 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+  var accordionButton = document.querySelectorAll(".accordion");
+
+  for (var i = 0; i < accordionButton.length; i++) {
+
+    accordionButton[i].addEventListener("click", function() {
+      // console.log("click");
+      this.classList.toggle("active");
+
+      var sibling = this.nextElementSibling;
+      if (sibling.style.display === "block") {
+        sibling.style.display = "none";
+      } else {
+        sibling.style.display = "block";
+      }
+/*
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+      }
+*/
+
+
+
+    });
+
+  }
 
 });
