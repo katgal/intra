4235e5190cba8ad9394c44c63ced0410b4278d15
Katarzyna Galas <katarzyna.galas2@gmail.com>
2017-10-12 14:27:12 +0200

    added return arrow to phones1,2 and started working on search engine

--- js/app.js
+++ js/app.js
@@ -1,36 +1,3 @@
-/*document.addEventListener("DOMContentLoaded", function() {
-
-  var accordionButton = document.querySelectorAll(".accordion");
-
-  for (var i = 0; i < accordionButton.length; i++) {
-
-    accordionButton[i].addEventListener("click", function() {
-      // console.log("click");
-      this.classList.toggle("active");
-
-      var sibling = this.nextElementSibling;
-      if (sibling.style.display === "block") {
-        sibling.style.display = "none";
-      } else {
-        sibling.style.display = "block";
-      }
-
-      var panel = this.nextElementSibling;
-      if (panel.style.maxHeight){
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-      }
-
-
-
-
-    });
-
-  }
-
-});
-*/
 $(document).ready(function() {
 
   function accordion() {
@@ -43,6 +10,17 @@ $(document).ready(function() {
     });
   }
   accordion();
+
+   function search(){
+
+
+   }
+   search();
+});
+
+
+
+
 /*
   var $root = $('html, body');
 
@@ -81,6 +59,3 @@ select.onchange = function(){
     window.scrollTo(0,top);
 };
 */
-
-
-});
